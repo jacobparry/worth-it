0a9b47d31a05f87378221053ee9a42fce6897b9b
Jacob Parry <japadm@divvypay.com>
2021-02-11 14:14:04 -0700

    single object

--- apps/api/lib/api_web/schema.ex
+++ apps/api/lib/api_web/schema.ex
@@ -1,7 +1,25 @@
 defmodule ApiWeb.Schema do
   use Absinthe.Schema
 
+  object :user do
+    field(:username, :string)
+    field(:password, :string)
+  end
+
   query do
+    field :users, list_of(:user) do
+      resolve(&user_list/3)
+    end
+
     # Nothing for now
+  end
+
+  defp user_list(_, _, _) do
+    {:ok,
+     [
+       %{username: "bob1", password: "strOngestEver!"},
+       %{username: "bob2", password: "strOngestEver!"},
+       %{username: "bob3", password: "strOngestEver!"}
+     ]}
   end
 end
