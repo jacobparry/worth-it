7e679180398a50b2fa0b6c6fbb3128ac5c13a24c
Jacob Parry <japadm@divvypay.com>
2021-02-25 13:46:44 -0700

    2.3-query-tracing

--- apps/api/mix.exs
+++ apps/api/mix.exs
@@ -32,7 +32,8 @@ defmodule Api.MixProject do
   defp deps do
     [
       {:absinthe, "~> 1.6"},
-      {:absinthe_plug, "~> 1.5"}
+      {:absinthe_plug, "~> 1.5"},
+      {:apollo_tracing, "~> 0.4.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
