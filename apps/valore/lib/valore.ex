405aed65d7b1d7ef3ca7aa822aa79ef6e2fc7555
Jacob Parry <japadm@divvypay.com>
2021-03-11 13:17:56 -0700

    3.0-setup-database

---
+++ apps/valore/lib/valore.ex
@@ -0,0 +1,18 @@
+defmodule Valore do
+  @moduledoc """
+  Documentation for `Valore`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Valore.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/valore/test/valore_test.exs
@@ -0,0 +1,8 @@
+defmodule ValoreTest do
+  use ExUnit.Case
+  doctest Valore
+
+  test "greets the world" do
+    assert Valore.hello() == :world
+  end
+end
